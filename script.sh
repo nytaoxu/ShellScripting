fbce9bbddde94d6d5c992aea5e7f137f1bbe2a3e
Tao <nytaoxu@gmail.com>
2019-04-03 08:46:15 -0400

    if statement

--- script.sh
+++ script.sh
@@ -6,5 +6,14 @@ echo hello world
 name="Infiniti"
 echo "Welcome, ${name}."
 
+#read
 read -p "Please enter your name: " name
 echo "The name you entered is $name."
+
+#if statement
+if [ $name == "Infiniti" ]
+then
+  echo "You are Infiniti."
+else
+  echo "You are someone else."
+fi
