77122206d63a41a9e281663ffb6071faec0fe4e1
Tao <nytaoxu@gmail.com>
2019-04-03 08:55:31 -0400

    comparison

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#echo command
+# echo command
 echo hello world
 
 name="Infiniti"
 echo "Welcome, ${name}."
 
-#read
+# read
 read -p "Please enter your name: " name
 echo "The name you entered is $name."
 
-#if-elif-else statement
+# if-elif-else statement
 if [ $name == "Infiniti" ]
 then
   echo "You are Infiniti."
@@ -19,4 +19,15 @@ then
   echo "You are Lexus."
 else
   echo "You are someone else."
+fi
+
+# comparison
+# -eq -ne -gt -ge -lt -le
+a=5
+b=7
+if [ $a -le $b ]
+then
+  echo "$a is less than or equal to $b."
+else
+  echo "$a is greater than $b."
 fi
