4518aed5a1b745eb408dba50c6ac460c026a83d1
Tao <nytaoxu@gmail.com>
2019-04-03 09:19:55 -0400

    file conditions

--- script.sh
+++ script.sh
@@ -31,3 +31,22 @@ then
 else
   echo "$a is greater than $b."
 fi
+
+# file conditions
+# -d file True if the file is a directory.
+# -e file True if the file exists (this is not particularly portable, thus -f is generally used.)
+# -f file True if the provided string is a file.
+# -g file True if the group id is set on a file.
+# -r file True if the file is readable.
+# -s file True if the file has a non-zero size.
+# -u      True if the user id is set on a file.
+# -w      True if the file is writable.
+# -x      True if the file is executable.
+
+file="./script.sh"
+if [ -f $file ]
+then
+  echo "$file is a file."
+else
+  echo "$file is not a file."
+fi
